6550febc6418ecce1f0e6d6d0693e691904f36e5
zinmohamed538-crypto <zinmohamed538@gmail.com>
2026-04-02 10:22:50 +0200

    c2

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import lombok.Data;
+import lombok.AllArgsConstructor;
 
+@Data
+@AllArgsConstructor
 public class Car {
+    private String model;
+    private String brand;
+    private int year;
+    private int price;
+    private String color;
+    private int quantity;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+
+        Car car1 = new Car("GLE", "Mercedes", 2020, 350000, "white", 5);
+        Car car2 = new Car("GLC", "Mercedes", 2023, 200000, "silver", 3);
+
+        System.out.println(car1);
+        System.out.println(car2.getColor());
+
+        int y = car1.getYear();
+        car1.setColor("black");
+
+        System.out.println("Are these objects equal? " + car1.equals(car2));
+        System.out.println("The hashCode of this object is: " + car1.hashCode());
+        System.out.println("The hashCode of this object is: " + car2.hashCode());
+    }
 }
